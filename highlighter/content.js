b5791493472db7105a94eea6cbfa5329c9d0bfa7
Konstantine Kahadze <85003299+kkahadze@users.noreply.github.com>
2023-06-20 12:54:22 -0400

    Service worker working

--- highlighter/content.js
+++ highlighter/content.js
@@ -1,10 +1,18 @@
-function logHighlightedText() {
+let port = chrome.runtime.connect({name: "highlightedTextPort"});
+
+function sendHighlightedText() {
   const selectedText = window.getSelection().toString();
   if (selectedText) {
     console.log(`Highlighted Text: ${selectedText}`);
+    port.postMessage({ text: selectedText });
   } else {
     console.log('No text selected');
   }
 }
 
-document.addEventListener('mouseup', logHighlightedText);
+document.addEventListener('mouseup', sendHighlightedText);
+
+// Listen for the unload event and disconnect the port when it fires.
+window.addEventListener('unload', function() {
+  port.disconnect();
+});
